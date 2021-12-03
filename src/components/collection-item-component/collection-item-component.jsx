287552d9b66ee55b867e7df605b518f548500830
Fabio Barros <fb040974@gmail.com>
2021-12-02 23:45:38 -0300

    update shop page

---
+++ src/components/collection-item-component/collection-item-component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export type CollectionItemComponentProps = {}
+
+export default function CollectionItemComponent({}: CollectionItemComponentProps) {
+    return (<div className="collection-item-component"/>);
+}
